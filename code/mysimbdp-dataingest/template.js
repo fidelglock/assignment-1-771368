8e0afa04c1ebfe6a90f8cb80dc30ef9b1f0eb6ac
fidelglock <50452324+fidelglock@users.noreply.github.com>
2019-10-11 07:21:50 +0200

    Update template.js

--- code/mysimbdp-dataingest/template.js
+++ code/mysimbdp-dataingest/template.js
@@ -5,7 +5,7 @@ exports.get = function(req, res) {
 
   var csv = json2csv({ data: "", fields: fields });
 
-  res.set("Content-Disposition", "attachment;filename=authors.csv");
+  res.set("Content-Disposition", "attachment;filename=IndoorLocDetDataset.csv");
   res.set("Content-Type", "application/octet-stream");
 
   res.send(csv);
